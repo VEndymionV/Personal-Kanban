a59103f127c005c93810a87ac357b2a3ee5cd613
Borys Chajdas <borys.chajdas@gmail.com>
2018-10-28 23:08:56 +0100

    - Dodany centralny StackedWidget w którym pojawiają się kolejne widoki po kliknięciu w odpowiedni przycisk. - Ikonka trybu nocnego reaguje na kliknięcie (zmiana ikony).

--- mainwindow.h
+++ mainwindow.h
@@ -7,6 +7,15 @@ namespace Ui {
 class MainWindow;
 }
 
+namespace StackedIndex {
+    const int SUMMARY = 0;
+    const int KANBAN = 1;
+    const int TIMELINE = 2;
+    const int STATS = 3;
+    const int NOTIFICATIONS = 4;
+    const int SETTINGS = 5;
+}
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -15,8 +24,33 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pBtn_Kanban_clicked();
+
+    void on_pBtn_Summary_clicked();
+
+    void on_pBtn_Timeline_clicked();
+
+    void on_pBtn_Stats_clicked();
+
+    void on_pBtn_Notifications_clicked();
+
+    void on_pBtn_Settings_clicked();
+
+    void on_pBtn_NightMode_clicked();
+
+    void on_pBtn_TodoAdd_clicked();
+
+    void on_pBtn_InProgressAdd_clicked();
+
+    void on_pBtn_DoneAdd_clicked();
+
 private:
     Ui::MainWindow *ui;
+    // icons:
+    QIcon nightModeIconOn;
+    QIcon nightModeIconOff;
+    bool nightMode;
 };
 
 #endif // MAINWINDOW_H
